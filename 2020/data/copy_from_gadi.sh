706038d8fec9aedc9b46f7ccb40621f61d1daa33
dougiesquire <dougiesquire@gmail.com>
2021-05-17 10:58:11 +1000

    update forecasts

--- 2020/data/copy_from_gadi.sh
+++ 2020/data/copy_from_gadi.sh
@@ -3,4 +3,8 @@
 #SBATCH --time=04:00:00
 #SBATCH --mem=8gb
 
+# Tranfer small files first
+rsync -avPS --max-size=1G  ds0092@gadi-dm.nci.org.au:/g/data/v14/ds0092/active_projects/hydro_tasmania/2020/data/*.zarr.zip
+
+# Then the rest
 rsync -avPS ds0092@gadi-dm.nci.org.au:/g/data/v14/ds0092/active_projects/hydro_tasmania/2020/data/*.zarr.zip .
