18a0468fa79fe401027ae3ef5e1a38b4e78a3bad
dougiesquire <dougiesquire@gmail.com>
2021-06-09 17:57:27 +1000

    update

--- 2020/data/copy_from_gadi.sh
+++ 2020/data/copy_from_gadi.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem=8gb
 
 # Tranfer small files first
-rsync -avPS --max-size=1G  ds0092@gadi-dm.nci.org.au:/g/data/v14/ds0092/active_projects/hydro_tasmania/2020/data/*.zarr.zip
+rsync -avPS --max-size=1G  ds0092@gadi-dm.nci.org.au:/g/data/v14/ds0092/active_projects/hydro_tasmania/2020/data/*.zarr.zip .
 
 # Then the rest
 rsync -avPS ds0092@gadi-dm.nci.org.au:/g/data/v14/ds0092/active_projects/hydro_tasmania/2020/data/*.zarr.zip .
